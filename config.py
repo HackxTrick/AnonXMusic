1d2569d3f6df124db4c5dfc332881cef1c8858be
HackxTrick <153048345+HackxTrick@users.noreply.github.com>
2024-06-26 13:39:00 +0530

    Update config.py
    Signed-off-by: HackxTrick <153048345+HackxTrick@users.noreply.github.com>

--- config.py
+++ config.py
@@ -31,8 +31,8 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
-    "GBAN_LOGS",
-    "https://t.me/CUTIE_GBAN"
+    "UPSTREAM_REPO",
+    "https://github.com/HackxTrick/AnonXMusic"
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
