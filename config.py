c89701a381bd3d8036c9ecdb65d94b788bbd9a49
HackxTrick <153048345+HackxTrick@users.noreply.github.com>
2025-07-02 22:40:39 +0530

    Update config.py
    Signed-off-by: HackxTrick <153048345+HackxTrick@users.noreply.github.com>

--- config.py
+++ config.py
@@ -39,12 +39,12 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/GAURAV_BOTS")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/INFINITE_EMPIRE")
-GBAN_CHAT = getenv("GBAN_CHAT", "https://t.me/Cutie_Gban")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Kirito_Bots")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/CutiexSupport")
+GBAN_CHAT = getenv("GBAN_CHAT", "https://t.me/Kirito_Gban")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
